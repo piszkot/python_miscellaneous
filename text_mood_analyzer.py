ce8d3e08279e36de2d1ead31853893f6d4cd1011
Piszkot <piszkot@piszkot.pl>
2026-03-18 08:00:05 +0100

    add textblob_info

--- text_mood_analyzer.py
+++ text_mood_analyzer.py
@@ -53,35 +53,3 @@ feedback_list = [
 if __name__ == "__main__":
     analyze_mood(feedback_list)
     print("")
-
-
-# ==========================================
-# TextBlob
-# ==========================================
-
-# TextBlob is a simple Python library for Natural Language Processing (NLP).
-# It provides easy-to-use tools for tasks such as:
-# - sentiment analysis (detecting if text is positive or negative)
-# - tokenization (splitting text into words or sentences)
-# - part-of-speech tagging (identifying nouns, verbs, etc.)
-# - translation and spell correction
-#
-
-# ==========================================
-# Corpora
-# ==========================================
-
-# Corpora (singular: corpus) are collections of text data used by NLP tools.
-# In the context of TextBlob, corpora include dictionaries, language models,
-# and datasets required for text analysis.
-#
-# TextBlob relies on external data (mainly from NLTK) to function properly,
-# so these corpora often need to be downloaded separately using:
-#     python -m textblob.download_corpora
-
-# ==========================================
-# In short:
-# ==========================================
-
-# - TextBlob = the tool/library
-# - Corpora = the data it uses to analyze language
